ba5707b6f1edf44383a4f393037986e193a3e738
Sushanth <sushanth.kulkarni262@gmail.com>
2026-03-18 20:27:12 +0530

    add cuda loggign

--- bigram_v2.py
+++ bigram_v2.py
@@ -9,6 +9,7 @@ max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {device}")
 eval_iters = 200
 n_embd = 384
 n_head = 6
